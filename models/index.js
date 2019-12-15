7515e1a877e3113cb9b4ccde0be18ff0d62d54ec
Bradley Pagano <bpagano5243@gmail.com>
2019-12-15 17:47:04 -0500

    adds the index model and adds HTML for saved.html

---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Articles: require("./Articles"),
+  Notes: require("./Notes"),
+  Saved: require("./Saved")
+};
--- server.js
+++ server.js
@@ -1,17 +1,66 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const db = require("./models");
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.caranddriver.com/news/").then(function(response) {
+    const $ = cheerio.load(response.data);
+
+    $(".full-item-content").each(function(i, element) {
+      const result = {};
+      result.title = $(this)
+        .childred("a")
+        .text();
+      result.body = $(this)
+        .children("p")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Articles.create(result)
+        .then(function(dbArticles) {
+          console.log(dbArticles);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.send("scrape complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Articles.find({}).then(function(dbArticles) {
+    res.json(dbArticles);
+  });
+});
+
+app.get("/saved", function(req, res) {
+  db.Saved.find({})
+    .populate("note")
+    .then(function(dbSaved) {
+      res.json(dbSaved);
+    });
+});
+
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("Server running on PORT: " + PORT);
+});
